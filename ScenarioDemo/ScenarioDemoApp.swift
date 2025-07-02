1789f2b460099aaf298802f2134096f7190385af
chanceadrian <chance.castaneda@me.com>
2025-07-02 11:21:40 -0700

    Initial Commit

---
+++ ScenarioDemo/ScenarioDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScenarioDemoApp.swift
+//  ScenarioDemo
+//
+//  Created by Chance Castaneda on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ScenarioDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
