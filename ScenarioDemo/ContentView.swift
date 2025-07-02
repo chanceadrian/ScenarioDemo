e240ceef29be5f2d4f082fc9f5fe0bff45260fa8
Evolone Layne <elayne297@gmail.com>
2025-07-02 15:02:32 -0400

    nav

--- ScenarioDemo/ContentView.swift
+++ ScenarioDemo/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Summary", systemImage: "newspaper") {
+                Text("Summary")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+            }
+            Tab("Proximate Cause", systemImage: "bolt") {
+                Text("Proximate Cause")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+            }
+            Tab("Downstream Impacts", systemImage: "arrow.down") {
+                Text("Downstream Impacts")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+            }
+            Tab("Mission Context", systemImage: "target") {
+                Text("Mission Context")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+            }
         }
-        .padding()
+        .tabViewStyle(.automatic)
     }
 }
 
